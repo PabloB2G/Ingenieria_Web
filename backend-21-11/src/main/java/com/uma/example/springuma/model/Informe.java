27b124c4e8ea781e4077d851a987818119f85c6b
ElArquitectorgo <mega2814s@gmail.com>
2023-11-23 16:27:36 +0100

    Añadido clase informe

---
+++ backend-21-11/src/main/java/com/uma/example/springuma/model/Informe.java
@@ -0,0 +1,56 @@
+package com.uma.example.springuma.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Informe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "prediccion")
+    private String prediccion;
+
+    @Column(name = "contenido", columnDefinition = "TEXT")
+    private String contenido;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getPrediccion() {
+        return prediccion;
+    }
+
+    public void setPrediccion(String prediccion) {
+        this.prediccion = prediccion;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    public Informe() {
+        // Constructor vacío necesario para JPA
+    }
+
+    public Informe(String prediccion, String contenido) {
+        this.prediccion = prediccion;
+        this.contenido = contenido;
+    }
+}
---
+++ backend-21-11/src/main/java/com/uma/example/springuma/model/InformeService.java
@@ -0,0 +1,44 @@
+package com.uma.example.springuma.model;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InformeService {
+
+    @Autowired
+    private RepositoryInforme repositoryInforme;
+
+    public List<Informe> getAllInformes() {
+        return repositoryInforme.findAll();
+    }
+
+    public Informe getInforme(Long id) {
+        return repositoryInforme.findById(id).orElse(null);
+    }
+
+    public Informe addInforme(Informe informe) {
+        return repositoryInforme.saveAndFlush(informe);
+    }
+
+    public void updateInforme(Informe informe) {
+        // Puedes implementar la lógica de actualización según tus necesidades
+        // Aquí se muestra un ejemplo básico:
+        Informe existingInforme = repositoryInforme.findById(informe.getId()).orElse(null);
+        if (existingInforme != null) {
+            existingInforme.setPrediccion(informe.getPrediccion());
+            existingInforme.setContenido(informe.getContenido());
+            repositoryInforme.save(existingInforme);
+        }
+    }
+
+    public void removeInforme(Informe informe) {
+        repositoryInforme.delete(informe);
+    }
+
+    public void removeInformeByID(Long id) {
+        repositoryInforme.deleteById(id);
+    }
+}
